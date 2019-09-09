179b357cd1feff5deb61c98daa8878262aa1e502
nima001 <1994974597@qq.com>
2019-09-09 21:17:42 +0800

    'python第一节课'

---
+++ helloword.py
@@ -0,0 +1,73 @@
+# print('helloword')
+#
+# a = input("请输入数据：")
+# print(a)
+#
+# print(type(a))
+#
+# print(type(int(a)))
+#
+# m,n=input("请输入两个数字：").split(",")
+#
+# print(m,n,end='@')
+
+# import math
+# a=int(input())
+# b=int(input())
+# c=int(input())
+#
+# s=(a+b+c)/2
+#
+# area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+# print("三角形面积：",area,end='')
+
+# import math
+
+# a = int(input())
+# b = int(input())
+#
+# s = a + b
+
+# print("和为：", s, end='')
+
+# import math
+# a,b,c=input().split()
+#
+# print(int(b)*int(b)-4*int(a)*int(c))
+#
+# s="Python语言简单易学"
+# print(s.encode('utf-8'))
+
+# 只要有一个运算数为浮点数，结果就是浮点数
+# print(6.0 / 4.0)
+# print(6.0 // 4.0)
+# print(6.0 % 4.0)
+
+# 计算商和余数
+# print(divmod(9, 2))
+# print(divmod(9.0, 2))
+
+# 2**2代表2的2次方
+
+# 复数
+# a=complex(4,-6)
+# print(a.real)#取实部
+# print(a.imag)#取虚部
+
+# 字符串
+# a="m"
+# b="a"
+# c="n"
+#
+# d=a+b+c
+# print(d)
+# print(c*5)
+
+# 多行字符串，'''XXX'''或者"""XXX""",三个单引号或者三个双引号
+print('''hello python
+    '人生苦短'
+我用Python''')
+
+print('hello python\n 人生苦短 \n 我用Python')
+
+print('\'hello python\'')
